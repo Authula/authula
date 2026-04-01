afbac10cb40502b1e3cef88f0fc6b161f1da8b6b
Tanvir Ahmed <mta.coder97@gmail.com>
2026-04-01 02:39:19 +0000

    chore: Removed TODO in config.go

--- plugins/access-control/types/config.go
+++ plugins/access-control/types/config.go
@@ -2,7 +2,6 @@ package types
 
 type AccessControlPluginConfig struct {
 	Enabled bool `json:"enabled" toml:"enabled"`
-	// TODO: Add a field to enable auto-cleanup of expired user roles and permissions
 }
 
 func (config *AccessControlPluginConfig) ApplyDefaults() {}
