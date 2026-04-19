c5769e3c18fdb9af6c5465a3534a1cca9c7beaf0
Tanvir Ahmed <54685928+m-t-a97@users.noreply.github.com>
2026-04-20 00:15:33 +0100

    refactor: Route Mappings (#70)

--- plugins/email-password/types/types.go
+++ plugins/email-password/types/types.go
@@ -7,12 +7,11 @@ import (
 )
 
 type EmailPasswordPluginConfig struct {
-	Enabled                  bool `json:"enabled" toml:"enabled"`
-	MinPasswordLength        int  `json:"min_password_length" toml:"min_password_length"`
-	MaxPasswordLength        int  `json:"max_password_length" toml:"max_password_length"`
-	DisableSignUp            bool `json:"disable_sign_up" toml:"disable_sign_up"`
-	RequireEmailVerification bool `json:"require_email_verification" toml:"require_email_verification"`
-	// Whether to automatically sign in the user after sign-up
+	Enabled                     bool          `json:"enabled" toml:"enabled"`
+	MinPasswordLength           int           `json:"min_password_length" toml:"min_password_length"`
+	MaxPasswordLength           int           `json:"max_password_length" toml:"max_password_length"`
+	DisableSignUp               bool          `json:"disable_sign_up" toml:"disable_sign_up"`
+	RequireEmailVerification    bool          `json:"require_email_verification" toml:"require_email_verification"`
 	AutoSignIn                  bool          `json:"auto_sign_in" toml:"auto_sign_in"`
 	SendEmailOnSignUp           bool          `json:"send_email_on_sign_up" toml:"send_email_on_sign_up"`
 	SendEmailOnSignIn           bool          `json:"send_email_on_sign_in" toml:"send_email_on_sign_in"`
